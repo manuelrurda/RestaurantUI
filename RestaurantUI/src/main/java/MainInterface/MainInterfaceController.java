f85203ab74ff30cef79d450b6ab23998e07348a5
manuelrurda <manuelrurda@gmail.com>
2022-01-06 21:53:34 -0600

    Main layout for table panel, working now on custom controls

--- RestaurantUI/src/main/java/MainInterface/MainInterfaceController.java
+++ RestaurantUI/src/main/java/MainInterface/MainInterfaceController.java
@@ -3,16 +3,26 @@ package MainInterface;
 import Main.Utils;
 import User.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import jdk.jshell.execution.Util;
+
+import java.util.List;
 
 public class MainInterfaceController {
 
     @FXML
     private ImageView fondaTistaLogo;
     @FXML
-    private MenuButton dropDownMenuButton;
+    private Label nameLabel;
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private Button adminMenuButton;
+    @FXML
+    private List<Button> tableButtons;
 
     private User user;
 
@@ -20,12 +30,34 @@ public class MainInterfaceController {
      * Metodo inicial de la escena, configura valores iniciales para los componentes.
      */
     public void initialize(){
+
+        // Logo Fonda Tista
         Image logo = new Image(Utils.formInputStreamFromURL("src/main/resources/Images/FondaTistaLogo.png"));
         fondaTistaLogo.setImage(logo);
+
+        // Icono Log-Out
+        Image logOutIcon = new Image(Utils.formInputStreamFromURL("src/main/resources/Images/LogOutIcon.png"));
+        ImageView lgiIV = new ImageView(logOutIcon);
+        lgiIV.setFitWidth(30);
+        lgiIV.setFitHeight(30);
+        logOutButton.setGraphic(lgiIV);
+
+        // Iconos de las mesas
+        /*for(Button b : tableButtons){
+            Image tableIcon = new Image(Utils.formInputStreamFromURL("src/main/resources/Images/TableIcon.png"));
+            ImageView tiIV = new ImageView(tableIcon);
+            tiIV.setFitWidth(80);
+            tiIV.setFitHeight(80);
+            b.setGraphic(tiIV);
+        }*/
+
         // Recibir usuario de escena de login.
         user = (User) Utils.receiveObject("currentUser");
-        dropDownMenuButton.setText(user.getFirstName());
+        nameLabel.setText(user.getFirstName());
         System.out.println(user.getClass());
+
+        // Habilitar menu admin
+        adminMenuButton.setVisible(user instanceof Admin);
     }
 
 }
